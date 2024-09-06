1cdc21019ac0659b2cdaded5451fb93239699520
Shubham Kumar Chaudhary <shubhamkumchaudhary@gmail.com>
2024-09-06 15:23:51 +0530

    change password

--- backend/dbConnection.js
+++ backend/dbConnection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const uri ="mongodb+srv://shubham:15025136S@cluster0.gk51g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri ="mongodb+srv://shubham:<password>cluster0.gk51g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 function dbConnection() {
   mongoose
